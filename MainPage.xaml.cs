6059a28ff7330e32ac670772ebc279bf14a7593c
Markame <mr.markame@gmail.com>
2024-09-08 10:03:06 -0300

    Adicionar arquivos de projeto.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using ZXing.Net.Maui;
+using ZXing.Net.Maui.Controls;
+
+namespace AppMaratona
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            var cameraView = new CameraBarcodeReaderView
+            {
+                IsDetecting = true,
+                Options = new BarcodeReaderOptions
+                {
+                    Formats = BarcodeFormat.QrCode,
+                    AutoRotate = true,
+                    //seta a opçao de identificar multiplos qrcodes
+                    Multiple = true
+                }
+            };
+
+            cameraView.BarcodesDetected += (s, e) =>
+            {
+                foreach (var barcode in e.Results)
+                {
+                    // Processar cada QR code detectado
+                    // barcode. value é o responsavel por receber o JSON
+                    Console.WriteLine($"QR Code detectado: {barcode.Value}");
+                    var json = barcode.Value;
+                   
+                }
+            };
+
+            Content = cameraView;
+        }
+        // criem uma função para enviar para o banco.
+        // criar uma classe e use a classe NewtonSoft para serializar o Json
+    }
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace AppMaratona
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
